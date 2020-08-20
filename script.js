fdcaf216a5b7f4079910cb9a6146c3cdfea4e458
math128ola <69984082+math128ola@users.noreply.github.com>
2020-08-20 13:57:17 -0400

    Website Files - Aug. 20th, 2020

---
+++ script.js
@@ -0,0 +1,11 @@
+//Simple JS code for interactive learn more button on CV page
+
+function revealMessage() {
+    document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+function revealMessage2() {
+    document.getElementById("hiddenMessage2").style.display = 'block';
+}
+
+
